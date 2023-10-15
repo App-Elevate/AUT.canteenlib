b22c9829d413d347312e7267cda888e863386901
tpkowastaken <protitom@gmail.com>
2023-10-15 09:44:18 +0200

    docs: vylepšen hlavní example

--- example/canteenlib_example.dart
+++ example/canteenlib_example.dart
@@ -1,13 +1,25 @@
 import 'package:canteenlib/canteenlib.dart';
 
+const String url = "kantyna.neco.cz";
+const String username = "uzivatel";
+const String heslo = "heslo123";
+
 void main(List<String> args) async {
-  Canteen c = Canteen(
-      "https://kantyna.neco.cz"); // vytvořit instanci kantýny, všechna komunikace probíhá skrz ni
+  /// Vytvoření instance kantýny. Všechna komunikace probíhá skrz ni.
+  Canteen canteenInstance = Canteen(url);
+
   try {
-    await c.login("uzivatel", "heslo"); // přihlásit se
-    var jidelnicek = await c.jidelnicekDen(den: DateTime.parse("2022-04-04"));
-    print((await c.ziskejUzivatele()).kredit);
-    var objednano = await c.objednat(jidelnicek.jidla[0]);
+    // příhlášení
+    await canteenInstance.login(username, heslo); // přihlásit se
+
+    // získání jídelníčku pro daný den i s cenami
+    var jidelnicek = await canteenInstance.jidelnicekDen(den: DateTime.parse("2022-04-04"));
+
+    // získání informací o uživateli, jako je například kredit
+    print((await canteenInstance.ziskejUzivatele()).kredit);
+
+    // objednání jídla
+    var objednano = await canteenInstance.objednat(jidelnicek.jidla[0]);
     print(objednano.objednano);
   } catch (e) {
     print("Při získávání informací nastala chyba: $e");
